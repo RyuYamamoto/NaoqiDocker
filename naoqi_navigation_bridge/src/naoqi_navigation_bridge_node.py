9192137d1a8f4d8a401d5a634e2d289f7212e035
RyuYamamoto <ryu.yamamoto04@g.softbank.co.jp>
2019-12-16 19:54:53 +0900

    人検知の可視化

--- naoqi_navigation_bridge/src/naoqi_navigation_bridge_node.py
+++ naoqi_navigation_bridge/src/naoqi_navigation_bridge_node.py
@@ -3,6 +3,7 @@ import rospy
 import tf2_ros
 import tf_conversions
 import geometry_msgs
+from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion, Pose
 from nav_msgs.msg import OccupancyGrid
 import almath
@@ -26,9 +27,10 @@ class ClientFactory:
 
 class NaoqiNavigationBridge:
     def __init__(self):
-        self.rate = rospy.Rate(2)
+        self.rate = rospy.Rate(100)
         self.map = rospy.Publisher("/naoqi_exploration_map", OccupancyGrid, queue_size=None)
-	self.connect("10.40.16.85")
+        self.people_marker = rospy.Publisher("/people_marker", Marker, queue_size=10)
+        self.connect("10.40.17.131")
 
     def connect(self, ip):
         try:
@@ -45,7 +47,11 @@ class NaoqiNavigationBridge:
                 except Exception as err2:
                     rospy.logerr(str(err2))
             self.navigation = self.session.service("ALNavigation")
+            self.perception = self.session.service("HumanPerception")
             self.motion = self.session.service("ALMotion")
+            self.boot_frame = self.motion._robotAtBootFrame()
+            self.map2robot = almath.Pose2D()
+            self.motion2robot = almath.Pose2D()
         except Exception as err3:
             rospy.logerr("Error when creating proxy:")
             rospy.logerr(str(err3))
@@ -111,16 +117,76 @@ class NaoqiNavigationBridge:
     def _robot_pose(self):
         try:
             robot_in_map = self.navigation.getRobotPositionInMap()
-            map2robot = almath.Pose2D(robot_in_map[0][0], robot_in_map[0][1], robot_in_map[0][2])
-            self._publish_tf(self._map2pose(map2robot), "robot_pose")
+            self.motion2robot = almath.Pose2D(self.motion.getRobotPosition(True))
+            self.map2robot = almath.Pose2D(robot_in_map[0][0], robot_in_map[0][1], robot_in_map[0][2])
+            self._publish_tf(self._map2pose(self.map2robot), "robot_pose")
         except Exception as err:
             rospy.logerr(str(err))
+
+    def _publish_marker(self, data):
+        marker_data = Marker()
+        marker_data.header.frame_id = "map"
+        marker_data.header.stamp = rospy.Time.now()
+
+        marker_data.ns = "basic_shapes"
+        marker_data.id = 0
+
+        marker_data.action = Marker.ADD
+
+        marker_data.pose.position.x = data.position.x
+        marker_data.pose.position.y = data.position.y
+        marker_data.pose.position.z = data.position.z
+        marker_data.pose.orientation.x = data.orientation.x
+        marker_data.pose.orientation.y = data.orientation.y
+        marker_data.pose.orientation.z = data.orientation.z
+        marker_data.pose.orientation.w = data.orientation.w
+
+        marker_data.color.r = 1.0
+        marker_data.color.g = 0.0
+        marker_data.color.b = 0.0
+        marker_data.color.a = 0.8
+
+        marker_data.scale.x = 0.5
+        marker_data.scale.y = 0.5
+        marker_data.scale.z = 2.0
+
+        marker_data.lifetime = rospy.Duration()
+
+        marker_data.type = 1
+
+        self.people_marker.publish(marker_data)
+
+    def _publish_perception(self):
+        human_around = self.perception.humansAroundPrivate.value()
+
+        for people in human_around:
+            human_frame = people.headFrame.value()
+            tf = human_frame.computeTransform(self.boot_frame)['transform']
+
+            w = float(tf['rotation']['w'])
+            x = float(tf['rotation']['x'])
+            y = float(tf['rotation']['y'])
+            z = float(tf['rotation']['z'])
+            people_rotation = almath.Quaternion(w, x, y, z)
+            rot_3d = almath.rotation3DFromQuaternion(people_rotation)
+
+            people_tf = almath.transformFromQuaternion(people_rotation)
+            people_tf.r1_c4 = float(tf['translation']['x'])
+            people_tf.r2_c4 = float(tf['translation']['y'])
+            people_tf.r3_c4 = float(tf['translation']['z'])
+            people_position_tf = almath.pose2DFromTransform(people_tf)
+            # map->robot, robot->world, world->people ==>> map->people
+            people_position = self.map2robot * self.motion2robot.inverse() * people_position_tf
+            self._publish_tf(self._map2pose(people_position), "people")
+            self._publish_marker(self._map2pose(people_position))
+            print people_position
 
     def run(self):
         rospy.loginfo("start")
         while not rospy.is_shutdown():
             self._publish_map()
             self._robot_pose()
+            self._publish_perception()
             self.rate.sleep()
 
 if __name__ == '__main__':
